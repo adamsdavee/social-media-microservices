fd6c30abe266c703420505050fe6bd5e7354d68a
adamsdave <davidkenechukwu591@gmail.com>
2026-01-21 16:16:43 +0100

    models

---
+++ identity-service/src/models/user.model.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose")
+const argon2 = require("argon2")
+
+const UserModel = new mongoose.Schema(
+   {
+      username: {
+         type: String,
+         required: true,
+         unique: true,
+         trim: true,
+      },
+
+      email: {
+         type: String,
+         required: true,
+         unique: true,
+         trim: true,
+         lowercase: true,
+      },
+
+      password: {
+         type: String,
+         required: true,
+      },
+
+      createdAt: {
+         type: Date,
+         default: Date.now(),
+      },
+   },
+   {
+      timestamps: true,
+   },
+)
+
+UserModel.pre("save", async function (next) {
+   if (this.isModified("password")) {
+      try {
+         this.password = await argon2.hash(this.password)
+      } catch (error) {
+         return next(error)
+      }
+   }
+})
+
+UserModel.methods.comparePassword = async function (candidatePassword) {
+   try {
+      return await argon2.verify(this.password, candidatePassword)
+   } catch (error) {
+      throw error
+   }
+}
+
+UserModel.index({ username: "text" })
+
+const User = mongoose.model("User", UserModel)
+
+module.exports = User
