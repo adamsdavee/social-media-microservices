d70ed465489cbfcb29045425984ca5842fd1e4a4
adamsdave <davidkenechukwu591@gmail.com>
2026-01-26 12:06:41 +0100

    post-router

---
+++ post-service/src/routes/post.route.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const { createPost } = require("../controllers/post.controller")
+const { authenticateRequest } = require("../middleware/auth.middleware")
+
+const postRouter = express.Router()
+
+// Because all the functions in the controller will require authentication of
+// requests rather than passing the auth request the normal way we do it like this so that every
+// request that comes into this route will first pass through the authenticateRequest middleware
+
+postRouter.use(authenticateRequest)
+
+postRouter.post("/create-post", createPost)
+
+module.exports = postRouter
